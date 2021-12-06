32f7d5eb927354f00f421ece894ffe62ae4ea941
Chris McKiernan <cmckier9@gmail.com>
2021-12-06 14:00:53 -0500

    Pretty sure I finished training function for naive faces

--- naive_bayes_face.py
+++ naive_bayes_face.py
@@ -24,6 +24,7 @@ def create_feature_list(file):
                 feature = []
                 
                 list.append(feature) 
+                #feature = []
                 line_num = 0
 
             for c in line: #Covert white-space to 0s and +/# to 1s
@@ -55,10 +56,81 @@ def train_bayes(feature_list, label_list):
     prob_face = num_faces / len(label_list)
     prob_not_face = 1 - prob_face
 
-    print(num_faces)
-    print(len(label_list) - num_faces)
-    print(prob_face)
-    print(prob_not_face)
-    print (len(feature_list))
+    face_feature_table_blanks = [0]*len(feature_list[0])
+    face_feature_table_pixels = [0]*len(feature_list[0])
+
+    not_face_feature_table_blanks = [0]*len(feature_list[0])
+    not_face_feature_table_pixels = [0]*len(feature_list[0])
+
+    i = 0
+    #j = 0
+    print("yo")
+    while i < len(feature_list):
+        if(label_list[i] == 1):
+            #print("face")
+            # do stuff with face_feature_table
+            j = 0
+            while j < len(face_feature_table_blanks):
+                #print(feature_list[i][j])
+                if (feature_list[i][j] == 0):
+                    face_feature_table_blanks[j] += 1
+                #j += 1
+                else:
+                    face_feature_table_pixels[j] += 1
+                j = j + 1
+        elif(label_list[i] == 0):
+            #print("not a face")
+            # do stuff with not_face_feature_table
+            j = 0
+            while j < len(not_face_feature_table_blanks):
+                #print(feature_list[i][j])
+                if (feature_list[i][j] == 0):
+                    not_face_feature_table_blanks[j] += 1
+                    #print("3")
+                else:
+                    not_face_feature_table_pixels[j] += 1
+                #     #print("4")
+                j = j + 1
+        i = i + 1
+
+    i = 0
+    while i < len(face_feature_table_blanks):
+        face_feature_table_blanks[i] /= num_faces
+        i += 1
+
+    i = 0
+    while i < len(face_feature_table_pixels):
+        face_feature_table_pixels[i] /= num_faces
+        i += 1
+
+    i = 0
+    while i < len(not_face_feature_table_blanks):
+        not_face_feature_table_blanks[i] /= (len(label_list) - num_faces)
+        i += 1
+
+    i = 0
+    while i < len(not_face_feature_table_pixels):
+        not_face_feature_table_pixels[i] /= (len(label_list) - num_faces)
+        i += 1
+
+    #print(len(feature_list) == len(label_list))
+    #print(len(feature_list))
+    #print(not_face_feature_table)
+    #print(451-num_faces)
+    #print(len(feature_list[0]))
+    #print(not_face_feature_table_blanks)
+    #print(face_feature_table2)
+    i = 0
+    while i < len(not_face_feature_table_blanks):
+        print(not_face_feature_table_blanks[i] + not_face_feature_table_pixels[i])
+        i += 1
+    # i = 1
+    # while i < len(feature_list):
+    #     print(np.array_equiv(feature_list[i], feature_list[i-1]))
+    #     i += 1
+    #print(not_face_feature_table)
+    #print(feature_list)
+
+
 
 train_bayes(create_feature_list(FACE_TRAIN_IMAGE_PATH), create_label_list(FACE_TRAIN_LABEL_PATH))

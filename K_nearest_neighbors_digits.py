4af1002bf6662f8fdf32b3b959e72f687634fea4
Evan227 <73192772+Evan227@users.noreply.github.com>
2021-12-14 20:03:04 -0500

    I think I'm finished

--- K_nearest_neighbors_digits.py
+++ K_nearest_neighbors_digits.py
@@ -2,6 +2,9 @@ import statistics
 from os import linesep
 import numpy as np
 import math
+import time
+import random
+from matplotlib import pyplot as plt
 
 
 
@@ -44,27 +47,35 @@ def create_label_list(file):
     return np.loadtxt(file, dtype=int)
 
 #make an array of height*length, "
-def train_classifier(feature_list, label_list):
+def train_classifier(x):
+    start_time = time.time()
     feature_list = create_feature_list(DIGIT_TRAIN_IMAGE_PATH)
     label_list = create_label_list(DIGIT_TRAIN_LABEL_PATH)
 
-    #every row is an image basically of the feature list
-
-    #label list if what each row is
-
-    #predicator is the amount of 1's in the array
-
     a = []
+    #longest_streak_a = []
+
+    random_list = random.sample(range(len(feature_list)), int((len(feature_list) * x * .01)))
 
     row_index = 0
     feature_list_index = 0
     how_many_pixels = 0
+    longest_streak = 0
+    temp_streak = 0
+    #total_pixels = 0
 
-    while (row_index < len(feature_list)):
+    while (row_index < len(random_list)):
         calculating_mean_of_row = []
-        while (feature_list_index < len(feature_list[row_index])):
-            if (feature_list[row_index][feature_list_index] == 1):
+        while (feature_list_index < len(feature_list[random_list[row_index]])):
+            if (feature_list[random_list[row_index]][feature_list_index] == 1):
                 how_many_pixels += 1
+                temp_streak+=1
+                #total_pixels+=1
+            else:
+                if temp_streak > longest_streak:
+                    longest_streak = temp_streak
+                temp_streak = 0
+
 
             if(feature_list_index != 0 and feature_list_index % 28 == 0):
                 if(how_many_pixels!= 0):
@@ -73,20 +84,38 @@ def train_classifier(feature_list, label_list):
 
             feature_list_index += 1
 
-        which_label = label_list[row_index]
+
+        which_label = label_list[random_list[row_index]]
         mean = statistics.mean(calculating_mean_of_row)
-        b = [mean, which_label]
+        median = statistics.median(calculating_mean_of_row)
+        minimum = min(calculating_mean_of_row)
+        maxi = max(calculating_mean_of_row)
+        b = [minimum, longest_streak, mean, median, maxi, which_label]
         a.append(b)
+        #b = [longest_streak, which_label]
+        #longest_streak_a.append(b)
+
 
         row_index += 1
         feature_list_index = 0
         how_many_pixels = 0
+        longest_streak = 0
+        #total_pixels=0
+
 
     a.sort()
-    return a
+    #longest_streak_a.sort()
 
-def test_classifier():
-    a = train_classifier(create_feature_list(DIGIT_TRAIN_IMAGE_PATH), create_label_list(DIGIT_TRAIN_LABEL_PATH))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+
+    print(*a)
+
+    return a, total_time
+
+def test_classifier(x):
+    a, total_time = train_classifier(x)
     # Feature list for testing data
     prediction_feature_list = create_feature_list(DIGIT_TEST_IMAGE_PATH)
 
@@ -98,12 +127,21 @@ def test_classifier():
     prediction_row_index = 0
     prediction_list_index = 0
     prediction_how_many_pixels = 0
+    longest_streak = 0
+    temp_streak = 0
+    total_pixels =0
 
     while (prediction_row_index < len(prediction_feature_list)):
         calculating_mean_of_row = []
         while (prediction_list_index < len(prediction_feature_list[prediction_row_index])):
             if (prediction_feature_list[prediction_row_index][prediction_list_index] == 1):
                 prediction_how_many_pixels += 1
+                temp_streak += 1
+                total_pixels+=1
+            else:
+                if temp_streak > longest_streak:
+                    longest_streak = temp_streak
+                temp_streak = 0
 
             if(prediction_list_index != 0 and prediction_list_index % 28 == 0):
                 if(prediction_how_many_pixels!=0):
@@ -114,30 +152,38 @@ def test_classifier():
 
         count = 0
         mean = statistics.mean(calculating_mean_of_row)
+        median = statistics.median(calculating_mean_of_row)
+        minimum = min(calculating_mean_of_row)
+        maxi = max(calculating_mean_of_row)
 
+        #average per row
+        #b = [minimum, longest_streak, mean, median, maxi, which_label]
         for x in a:
-            if(x[0] >= mean):
+            if(x[0] >= minimum and x[1] >= longest_streak and x[2] >= mean and x[3] >= median and x[4] >= maxi):
                 count+=1
                 break
             count+=1
 
         b = []
 
-        upper_bound = count + 5  # 10 nearest neighbors
-        if (count - 5 >= 0):
+        upper_bound = count + 5# 10 nearest neighbors
+        if (count -5>= 0):
             count -= 5
         else:
             count = 0
 
         while (count < len(a) and count <= upper_bound):
-            b.append(a[count][1])
+            b.append(a[count][5])
             count += 1
+
 
         predictions[prediction_row_index] = most_frequent(b)
 
         prediction_row_index += 1
         prediction_list_index = 0
         prediction_how_many_pixels = 0
+        longest_streak = 0
+        total_pixels=0
 
     num_correct = 0
     x = 0
@@ -146,8 +192,77 @@ def test_classifier():
             num_correct += 1
         x += 1
 
-    print("Accuracy: ", num_correct / len(predictions))
+    return num_correct / len(predictions)
 
-test_classifier()
+def get_stats():
+    # list of averages/std/time for each percentage of data points,
+    #i.e: mean_list[i] is the mean for 10% of the training set
+    mean_list = []
+    std_list = []
+    time_list = []
+    for train_percentage in range(10, 110, 10):
+        acc = []
+        total_time = []
+        for i in range(4):
+            a, time_spent = train_classifier(train_percentage)
+            total_time.append(time_spent)
+            acc.append(test_classifier(train_percentage))
+            print("accuracy: ",i, " ", test_classifier(train_percentage))
+
+        mean_list.append(np.mean(acc))
+        std_list.append(np.std(acc))
+        time_list.append(np.mean(total_time))
+    return mean_list, std_list, time_list
+
+
+def graph(mean_list, std_list, time_list, display):
+    train_percentage = [.10, .20, .30, .40, .50, .60, .70, .80, .90, 1]
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
+    # Mean Accuracy vs Data points
+    if display == 1:
+        ax.plot(train_percentage, mean_list, color="blue")
+        plt.title("DIGITS: Accuracy vs Data Points")
+        plt.xlabel("Data Points (%)")
+        plt.ylabel("Average Accuracy (%)")
+        ax.set_yticklabels(['{:.0%}'.format(x) for x in ax.get_yticks()])
+    # Standard Deviation vs Data Points
+    if display == 2:
+        ax.plot(train_percentage, std_list, color="green")
+        plt.title("DIGITS: Standard Deviation vs Data Points")
+        plt.xlabel("Data Points (%)")
+        plt.ylabel("Average Standard Deviation (%)")
+        ax.set_yticklabels(['{:.0%}'.format(x) for x in ax.get_yticks()])
+    # Time vs Data points
+    if display == 3:
+        ax.plot(train_percentage, time_list, color="red")
+        plt.title("DIGITS: Time to Train vs Data Points")
+        plt.xlabel("Data Points (%)")
+        plt.ylabel("Time Needed to Train (s)")
+
+    ax.set_xticklabels(['{:.0%}'.format(x) for x in ax.get_xticks()])
+    ax.plot()
+    plt.show()
+
+mean_list, std_list, time_list = get_stats()
+graph(mean_list, std_list, time_list, 1)
+graph(mean_list, std_list, time_list, 2)
+graph(mean_list, std_list, time_list, 3)
+'''
+a = [0] * 10
+a[0] = test_classifier(10)
+a[1] = test_classifier(20)
+a[2] = test_classifier(30)
+a[3] = test_classifier(40)
+a[4] = test_classifier(50)
+a[5] = test_classifier(60)
+a[6] = test_classifier(70)
+a[7] = test_classifier(80)
+a[8] = test_classifier(90)
+a[9] = test_classifier(100)
+#train_classifier(100)
+print(test_classifier(100))'''
 
 
--- K_nearest_neighbors_faces.py
+++ K_nearest_neighbors_faces.py
@@ -1,7 +1,10 @@
 import statistics
+import time
 from os import linesep
 import numpy as np
 import math
+from matplotlib import pyplot as plt
+import random
 
 
 
@@ -45,19 +48,23 @@ def create_label_list(file):
     return np.loadtxt(file, dtype=int)
 
 def train_classifier(x):
+    start_time = time.time()
     feature_list = create_feature_list(FACE_TRAIN_IMAGE_PATH)
     label_list = create_label_list(FACE_TRAIN_LABEL_PATH)
 
     a = []
 
+    random_list = random.sample(range(len(feature_list)), int((len(feature_list)*x*.01)))
+
+
     row_index = 0
     feature_list_index = 0
     how_many_pixels = 0
 
-    while (row_index < len(feature_list)*x):
+    while (row_index < len(random_list)):
         calculating_mean_of_row = []
-        while (feature_list_index < len(feature_list[row_index])):
-            if (feature_list[row_index][feature_list_index] == 1):
+        while (feature_list_index < len(feature_list[random_list[row_index]])):
+            if (feature_list[random_list[row_index]][feature_list_index] == 1):
                 how_many_pixels += 1
 
             if (feature_list_index != 0 and feature_list_index % 60 == 0):
@@ -67,7 +74,7 @@ def train_classifier(x):
 
             feature_list_index += 1
 
-        which_label = label_list[row_index]
+        which_label = label_list[random_list[row_index]]
         mean = statistics.mean(calculating_mean_of_row)
         b = [mean, which_label]
         a.append(b)
@@ -77,11 +84,16 @@ def train_classifier(x):
         how_many_pixels = 0
 
     a.sort()
-    return a
+
+    end_time = time.time()
+
+    total_time = end_time - start_time
+
+    return a, total_time
 
 def test_classifier(x):
 
-    a = train_classifier(x)
+    a, total_time = train_classifier(x)
 
     # Feature list for testing data
     prediction_feature_list = create_feature_list(FACE_TEST_IMAGE_PATH)
@@ -144,24 +156,82 @@ def test_classifier(x):
 
     #print("Accuracy: ", num_correct / len(predictions))
 
-    return num_correct / len(predictions)
+    return (num_correct / len(predictions))
+
+def get_stats():
+    # list of averages/std/time for each percentage of data points,
+    #i.e: mean_list[i] is the mean for 10% of the training set
+    mean_list = []
+    std_list = []
+    time_list = []
+    for train_percentage in range(10, 110, 10):
+        acc = []
+        total_time = []
+        for i in range(5):
+            a, time_spent = train_classifier(train_percentage)
+            total_time.append(time_spent)
+            acc.append(test_classifier(train_percentage))
+            print("accuracy: ",i, " ", test_classifier(train_percentage))
+
+        mean_list.append(np.mean(acc))
+        std_list.append(np.std(acc))
+        time_list.append(np.mean(total_time))
+    return mean_list, std_list, time_list
 
 
-''' 
+def graph(mean_list, std_list, time_list, display):
+    train_percentage = [.10, .20, .30, .40, .50, .60, .70, .80, .90, 1]
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
+    # Mean Accuracy vs Data points
+    if display == 1:
+        ax.plot(train_percentage, mean_list, color="blue")
+        plt.title("FACE: Accuracy vs Data Points")
+        plt.xlabel("Data Points (%)")
+        plt.ylabel("Average Accuracy (%)")
+        ax.set_yticklabels(['{:.0%}'.format(x) for x in ax.get_yticks()])
+    # Standard Deviation vs Data Points
+    if display == 2:
+        ax.plot(train_percentage, std_list, color="green")
+        plt.title("FACE: Standard Deviation vs Data Points")
+        plt.xlabel("Data Points (%)")
+        plt.ylabel("Average Standard Deviation (%)")
+        ax.set_yticklabels(['{:.0%}'.format(x) for x in ax.get_yticks()])
+    # Time vs Data points
+    if display == 3:
+        ax.plot(train_percentage, time_list, color="red")
+        plt.title("FACE: Time to Train vs Data Points")
+        plt.xlabel("Data Points (%)")
+        plt.ylabel("Time Needed to Train (s)")
+
+    ax.set_xticklabels(['{:.0%}'.format(x) for x in ax.get_xticks()])
+    ax.plot()
+    plt.show()
+
+
+mean_list, std_list, time_list = get_stats()
+graph(mean_list, std_list, time_list, 1)
+graph(mean_list, std_list, time_list, 2)
+graph(mean_list, std_list, time_list, 3)
+
+'''
 Creates a training list of the average amount of "filled pixels" per row of the image
 and then makes a prediction whether it's a face or not based by calculating the average amount of
 "filled pixels" in the given image and then finding the 40 nearest neighbors or the 50 closest 
 images in the training list that have similar amount of "filled" pixels
 '''
-a = [0] * 10
-a[0] = test_classifier(0.1)
-a[1] = test_classifier(0.2)
-a[2] = test_classifier(0.3)
-a[3] = test_classifier(0.4)
-a[4] = test_classifier(0.5)
-a[5] = test_classifier(0.6)
-a[6] = test_classifier(0.7)
-a[7] = test_classifier(0.8)
-a[8] = test_classifier(0.9)
-a[9] = test_classifier(1)
-print(*a)
+
+'''a = [0] * 10
+a[0] = test_classifier(10)
+a[1] = test_classifier(20)
+a[2] = test_classifier(30)
+a[3] = test_classifier(40)
+a[4] = test_classifier(50)
+a[5] = test_classifier(60)
+a[6] = test_classifier(70)
+a[7] = test_classifier(80)
+a[8] = test_classifier(90)
+a[9] = test_classifier(100)
+print(*a)'''
